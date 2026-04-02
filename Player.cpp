0d2028165afbb9cf63eb1afaf3eea6fec77f5cec
Nattapat Chantarojsiri <u3652561@connect.hku.hk>
2026-04-02 17:14:42 +0800

    fixed attack direction not working as intended

--- Player.cpp
+++ Player.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 void Player::move(int dx, int dy, const Map& map){
+    lastX = dx;
+    lastY = dy;
+    
     if(map.isWalkable(x + dx, y + dy)){
         x += dx;
         y += dy;
-
-        lastX = dx;
-        lastY = dy;
     }
 }
