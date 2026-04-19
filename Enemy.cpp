cb80afe1b7a132ca7c8097bb9c53b9b54393f51a
Nu7h <nuthbanrodfai@gmail.com>
2026-04-19 15:01:25 +0800

    resolve conflict

--- Enemy.cpp
+++ Enemy.cpp
@@ -7,40 +7,6 @@
 
 using namespace std;
 
-class baseEnemy
-{
-    public:
-    void idle(){
-
-    }
-    void moving(){
-
-    }
-    void takingDMG(){
-        
-    }
-    int attacking(){
-
-    }
-    
-};
-
-class  Slime : public baseEnemy
-{
-    private:
-    int hp = 100;
-    int min_atk = 0;
-    int max_atk = 10;
-};
-
-class Boss : public baseEnemy
-{
-    private:
-    int hp = 1000;
-    int min_atk = 10;
-    int max_atk = 40;
-    int healing = 10;
-};
 int max_approaching_tiles = 10;
 const int slimeShootCooldownTicks = 20;
 
